c28f763c641edd61c5d267597ed40758a95943ba
Preshos <domingoprecious83@gmail.com>
2023-11-30 11:00:45 +0800

    landing page

--- src/app/screen/landingpage/landingpage.page.ts
+++ src/app/screen/landingpage/landingpage.page.ts
@@ -1,43 +1,72 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Swiper } from 'swiper';
 import { AnimationOptions } from 'ngx-lottie';
+
 @Component({
   selector: 'app-landingpage',
   templateUrl: './landingpage.page.html',
   styleUrls: ['./landingpage.page.scss'],
 })
 export class LandingpagePage implements OnInit {
+  @ViewChild('swiper') swiper?: ElementRef<{ swiper: Swiper }>;
 
-  @ViewChild('swiper') swiper?:ElementRef  <{swiper:Swiper}>;
-  
+  index = 0;
 
-  // logActiveIndex() {
-  //   console.log(this.swiper?.nativeElement.swiper.activeIndex);
-  // }
-  
-  slidePrev(){
+  slidePrev() {
     this.swiper.nativeElement.swiper.slidePrev();
-  
   }
-  slideNext(){
+
+  slideNext() {
     this.swiper.nativeElement.swiper.slideNext();
   }
-  
-  constructor() { }
 
-  ngOnInit() {
-  }
+  constructor() {}
 
-  option: AnimationOptions = {path : '/assets/landing page pictures/learning.json'}
-  option1: AnimationOptions = {path : '/assets/landing page pictures/herbs.json'}
-  option2: AnimationOptions = {path : '/assets/landing page pictures/firstaid_phone.json'}
-  grow: AnimationOptions = {path : '/assets/landing page pictures/grow_plant.json'}
-  
+  ngOnInit() {}
+
+  option: AnimationOptions = {
+    path: '/assets/landing page pictures/learning.json',
+    
+  };
+
+  option1: AnimationOptions = {
+    path: '/assets/landing page pictures/herbs.json',
+    
+  };
+
+  option2: AnimationOptions = {
+    path: '/assets/landing page pictures/firstaid_phone.json',
+    
+  };
+
+  grow: AnimationOptions = {
+    path: '/assets/landing page pictures/grow_plant.json',
+    autoplay: false,
+  };
 
   titles = [
-    { options : this.option2, url:('/assets/landing page pictures/hand-heart.jpg') , message : "Learn essential life-saving techniques."},
-    { options : this.option1, url:('/assets/landing page pictures/hand-plant.jpg') , message : "Discover the Healing Power of Nature!" },
-    { options : this.option ,url:('/assets/landing page pictures/hand-bulb.jpg')  , message : " Quick tips for easy recall during emergencies" },
-  ]
+    {
+      options: this.option2,
+      url: '/assets/landing page pictures/hand-heart.jpg',
+      message: 'Learn essential life-saving techniques.',
+    },
+    {
+      options: this.option1,
+      url: '/assets/landing page pictures/hand-plant.jpg',
+      message: 'Discover the Healing Power of Nature!',
+    },
+    {
+      options: this.option,
+      url: '/assets/landing page pictures/hand-bulb.jpg',
+      message: 'Quick tips for easy recall during emergencies',
+    },
+  ];
 
+  slideDidChange() {
+
+    this.index = this.swiper?.nativeElement.swiper.activeIndex ?? 0;
+    // Update autoplay for grow animation
+    this.grow.autoplay = this.index === 1;
+   
+  }
 }
